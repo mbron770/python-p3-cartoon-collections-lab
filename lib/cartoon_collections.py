c8b9f1f3230d63a669ce0697ee5a8f9c80190c47
mbron770 <mbronfin.tsd@gmail.com>
2023-07-05 14:32:28 -0400

    complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,28 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves=["Doc", "Dopey", "Bashful", "Grumpy"]):
+    for i, name in enumerate(dwarves, start=1):
+        print(f'{i}. {name}')
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planeteer_calls=["earth", "wind", "fire", "water", "heart"]):
+    return [f'{name.title()}!' for name in planeteer_calls]
 
-def find_the_cheese():
-    pass
+
+def long_planeteer_calls(assorted_words=["axe", "earth", "wind", "fire"]):
+    # for word in assorted_words:
+    #     if(len(word) >= 4):
+    #         return True
+    # return False
+    return any(len(word) > 4 for word in assorted_words)
+
+
+def find_the_cheese(lst = ["crackers", "gouda", "thyme"] ):
+    cheeses = ['gouda', 'cheddar', 'camembert' ]
+    
+    for food in lst:
+        if food in cheeses: 
+            return food 
+    return None 
+    
+
+
+    
